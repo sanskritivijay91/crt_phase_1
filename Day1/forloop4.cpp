7604df0e86808228b79345f8255fc85b6bd35bed
Sanskriti <sanskritivijay91@gmail.com>
2026-05-04 11:41:36 +0530

    Normal commit

--- Day1/forloop4.cpp
+++ Day1/forloop4.cpp
@@ -7,10 +7,10 @@ int main() {
      for (int i = 1; i<= n; i++){
          for (int j =1; j<= n; j++){
              if (i==1 || i==n || j==1 || j==n ){
-                 cout<< "*";
+                 cout<< " *";
              }
              else {
-                 cout<<  " ";
+                 cout << "  ";
              }
          }
          cout<< endl;
