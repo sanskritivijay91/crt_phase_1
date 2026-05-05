5f508aabb5b735e537ba0462f91f79f2a652b9d7
Sanskriti <sanskritivijay91@gmail.com>
2026-05-05 14:31:35 +0530

    Normal commit

---
+++ Day5/pointers_swap.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+void swap ( int *a , int *b){
+    int temp = *a;   
+    *a = *b;
+    *b = temp;
+}
+ int main(){
+    int a , b;
+    cout << " enter the numbers:";
+    cin >> a >> b;
+    cout << " before swapping: a=" << a << " b=" << b << endl;
+    swap (&a , &b);
+    cout << " after swapping: a=" << a << " b=" << b << endl;
+    return 0;
+ }   
