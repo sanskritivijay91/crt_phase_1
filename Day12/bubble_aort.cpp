e0b0fb4c47eaab07811c7dcfd749300612be665b
Sanskriti <sanskritivijay91@gmail.com>
2026-05-12 08:56:42 +0530

    Normal commit

--- Day12/bubble_aort.cpp
+++ Day12/bubble_aort.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 using namespace std;
 
-void swap_array (int a, int b ){
+void swap_array (int &a, int &b ){   
     int temp = a;
     a = b;
     b = temp;
@@ -11,7 +11,7 @@ void Bubble_sort(int nums[], int n)
 {
     for (int i =0; i< n-1; i++){
     bool flag = false;
-    for(int j =0; j< n- i -1; j++ )
+    for(int j =0; j < n- i -1; j++ )
     {
         if (nums[j] > nums[j+1])
         {
@@ -31,7 +31,7 @@ void Bubble_sort(int nums[], int n)
 
 int main()
 {   
-    int arr[5] ={1,2,3,4,5};
+    int arr[5] ={1,2,3,5,4};  
     int size = sizeof(arr) / sizeof(arr[0]);
 
     Bubble_sort(arr, size);
